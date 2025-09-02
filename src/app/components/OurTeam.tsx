60845fe3dc13a3e828422d2a75765e7cb340318d
Gonzalo Lorenzon <gonzalo@air-de-gonzalo.home>
2025-09-02 15:16:07 -0300

    Last fix darinka photo

--- src/app/components/OurTeam.tsx
+++ src/app/components/OurTeam.tsx
@@ -13,7 +13,7 @@ const teamMembers = [
   {
     name: "Darinka Polich",
     role: "Content Creator, Community Manager",
-    img: "/images/Darinka.JPG",
+    img: "/images/Darinka-1.jpg",
     description: `En Bastra Studio gestiono redes sociales y creo contenido que conecta. Diseño estrategias, produzco fotos, videos y campañas alineadas con la esencia de la marca. Cada publicación tiene un objetivo claro: atraer, inspirar, vender o fidelizar.`,
   },
   {
